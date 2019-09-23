a4b27684513d8df8f06ccd235deb2ab3e22472ad
Seba <sebithaa1999@gmail.com>
2019-09-23 01:27:20 -0300

    Ahora dibuja los puntos en el mapa.

--- JmapViewer/src/principal/Window.java
+++ JmapViewer/src/principal/Window.java
@@ -2,17 +2,22 @@ package principal;
 
 import java.awt.EventQueue;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 
+import txtData.LectorTxt;
 
 public class Window {
 
 	private JFrame frame;
-	private static JMapViewer map;
-	private  txtData.ReaderTxt txt;
+	private static JMapViewer mapa;
+	private LectorTxt lector;
+	private ArrayList<Double> lista;
 
 	/**
 	 * Launch the application.
@@ -21,12 +26,11 @@ public class Window {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
 					Window window = new Window();
 					window.frame.setVisible(true);
-					window.frame.add(map);
-					//Coordinate bsAs = new Coordinate (59.2238,34.2237);
- 					//mapa.setDisplayPosition(bsAs,3);
+					window.frame.add(mapa);
+					Coordinate bsAs = new Coordinate (-34.5237,-58.7038);
+ 					mapa.setDisplayPosition(bsAs, 10);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -48,15 +52,16 @@ public class Window {
 	 */
 	private void initialize() throws IOException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 650, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		map = new JMapViewer(); 
-		txt = new txtData.ReaderTxt();
-		
-		System.out.println(txt.read("instancia1"));
-		
+		mapa = new JMapViewer();
+		lector = new LectorTxt();
+		lista = new ArrayList<Double>();
+		lista = lector.leerTxt("instancia1");
+		for(int i = 1; i < lista.size(); i++) {
+			MapMarker coordenada = new MapMarkerDot(lista.get(i-1),lista.get(i));
+			mapa.addMapMarker(coordenada);
+		}
+			
 	}
-	private void setPoint() {	
-	}
-
 }
