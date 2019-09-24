d148ae94053cc63c966c36dc904d9bd43c2349c4
Sebastian Pintos <47344141+SebastianPintos@users.noreply.github.com>
2019-09-24 16:28:03 -0300

    Update Grafo.java

--- JmapViewer/src/grafo/Grafo.java
+++ JmapViewer/src/grafo/Grafo.java
@@ -1,64 +1,55 @@
 package grafo;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 
+import javafx.util.Pair;
+
 public class Grafo {
 	// Representamos el grafo por su matriz de adyacencia
-	private Coordinate[] A;
-	private Double[][] distancias;
+	private Double[][] A;
 
-	// Crea un grafo completo
-	public  Grafo(HashMap<Double, Double> Coordenadas) {
-		Set<Entry<Double, Double>> c = Coordenadas.entrySet();
-		A = new Coordinate[Coordenadas.size()];
-		distancias = new Double[Coordenadas.size()][Coordenadas.size()];
-		int i = 0;
-
-		for (Map.Entry<Double, Double> m : c) {
-			A[i] = new Coordinate(m.getKey(), m.getValue());
-			i++;
-		}
-		setDistancias();
+	// El conjunto de vértices está fijo
+	public Grafo(int vertices) {
+		A = new Double[vertices][vertices];
 	}
 
-	public void setDistancias() {
-		for (int i = 0; i < tamano(); i++) {
-			for (int j = 0; j < tamano(); j++) {
-				calcularPeso(i, j);
+	// Crea un grafo completo
+	public void GrafoCompleto(int vertices, ArrayList<Coordinate> lista) {
+		for (int i = 0; i < vertices; i++) {
+			for (int j = 0; j < vertices; j++) {
+				if (i != j) {
+					Double deltax = lista.get(i).getLat() - lista.get(j).getLat();
+					Double deltay = lista.get(i).getLon() - lista.get(j).getLon();
+					A[i][j] = A[j][i] = Math.sqrt(deltax*deltax + deltay*deltay);
+				}
 			}
 		}
 	}
-	
-	private void calcularPeso(int i, int j) {
-		if (i != j) {
-			Double delta_i=Math.abs(A[i].getLat()-A[i].getLon());
-			Double delta_j=Math.abs(A[j].getLat()-A[j].getLon());
-			
-			Double distanciaEuclidea = Math.sqrt((delta_i*delta_i)+(delta_j*delta_j));
-			
-			distancias[i][j] = distanciaEuclidea;
-			distancias[j][i] = distanciaEuclidea;
-		}
-		else {
-			distancias[i][j] = -1.0;
-			distancias[j][i] = -1.0;
-		}
+
+	public Double distancia(int i, int j) {
+		return A[i][j];
+	}
+
+	// Operaciones sobre aristas
+	public void agregarArista(int i, int j, Coordinate coordenadas1, Coordinate coordenadas2) {
+		verificarIndices(i, j);
+		Double deltax = coordenadas1.getLat() - coordenadas2.getLat();
+		Double deltay = coordenadas1.getLon() - coordenadas2.getLon();
+		A[i][j] = A[j][i] = Math.sqrt(deltax*deltax + deltay*deltay);
 	}
 
 	public void borrarArista(int i, int j) {
 		verificarIndices(i, j);
-		distancias[i][j] = distancias[j][i] = -1.0;
+		A[i][j] = A[j][i] = 0.0;
 	}
 
 	public boolean existeArista(int i, int j) {
 		verificarIndices(i, j);
-		return distancias[i][j] != -1;
+		return A[i][j] != 0 ? true : false;
 	}
 
 	// Vecinos de un vertice
@@ -73,28 +64,22 @@ public class Grafo {
 		return ret;
 	}
 
-	public Double getArista(int i,int j){
-		verificarIndices(i,j);
-		return distancias[i][j];
-	}
 	// Cantidad de vertices
 	public int tamano() {
 		return A.length;
 	}
 
-	// Lanza excepciones si los �ndices no son v�lidos
+	// Lanza excepciones si los índices no son válidos
 	private void verificarIndices(int i, int j) {
 		verificarVertice(i);
 		verificarVertice(j);
 
 		if (i == j)
-			throw new IllegalArgumentException(
-					"No existen aristas entre un vertice y si mismo! vertice = "+ i);
+			throw new IllegalArgumentException("No existen aristas entre un vertice y si mismo! vertice = " + i);
 	}
 
 	private void verificarVertice(int i) {
 		if (i < 0 || i >= tamano())
-			throw new IllegalArgumentException("El vertice " + i
-					+ " no existe!");
+			throw new IllegalArgumentException("El vertice " + i + " no existe!");
 	}
 }
