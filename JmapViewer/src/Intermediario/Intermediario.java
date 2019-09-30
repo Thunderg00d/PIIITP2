2012c1d08ec2aaebc105455398dc1bfa6b624c24
Dell <Dell@192.168.0.3>
2019-09-30 15:18:09 -0300

    Agregada Excepcion

--- JmapViewer/src/Intermediario/Intermediario.java
+++ JmapViewer/src/Intermediario/Intermediario.java
@@ -17,7 +17,7 @@ public class Intermediario implements Cloneable{
 	}
 	
 	public void  setCoordenadas() throws IOException{
-		String[] archivos = {"instancia1"};
+		String[] archivos = {"instancia5"};
 		ArrayList<String> coordesTemp = new ArrayList<String>();
 		for(String s : archivos) {
 			for(String j : lector.LeerArchivo(s)) {
