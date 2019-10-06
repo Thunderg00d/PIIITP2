b3fcfb090d4abecf69133938a12f011b583f0d14
Dell <Dell@192.168.0.3>
2019-10-06 19:10:08 -0300

    Agregue test

--- JmapViewer/src/grafoTest/Assert.java
+++ JmapViewer/src/grafoTest/Assert.java
@@ -2,7 +2,12 @@
 	package grafoTest;
 
 	import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import grafo.Grafo;
+import javafx.util.Pair;
+import ordenar.Ordenar;
 
 	public class Assert
 	{
@@ -13,6 +18,13 @@ import grafo.Grafo;
 				for(int j=0;j<grafo1.tamano() && j!=i;j++) {
 					assertEquals(grafo1.getArista(i,j),grafo2.getArista(i, j));
 				}
+			}
+		}
+		public static void iguales(Ordenar ordenar, List<Pair<Integer,Integer>> indicesResultantes,Double[]distancias) {
+			assertEquals(ordenar.tamano(),indicesResultantes.size(),distancias.length);
+			for(int i=0;i<ordenar.tamano();i++) {
+				assertEquals(ordenar.getDistancia(i),distancias[i]);
+				assertEquals(ordenar.indices(i),indicesResultantes.get(i));
 			}
 		}
 	}
--- JmapViewer/src/grafoTest/OrdenarTest.java
+++ JmapViewer/src/grafoTest/OrdenarTest.java
@@ -1,15 +1,15 @@
-package ordenar;
+package grafoTest;
 
-import static org.junit.Assert.*;
+
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
 import javafx.util.Pair;
+import ordenar.Ordenar;
 
 public class OrdenarTest {
-
 	@Test
 	public void test() {
 		Double[]dis=new Double[4];
@@ -38,20 +38,7 @@ public class OrdenarTest {
 		indiceEsperado.add(new Pair<Integer,Integer>(1,2));
 		indiceEsperado.add(new Pair<Integer,Integer>(3,0));
 		
-		
-		assertEquals(ord.getDistancia(0),Double.valueOf(1.0));
-		assertEquals(ord.indices(0),new Pair<Integer,Integer>(2,3));
-		
-		assertEquals(ord.getDistancia(1),Double.valueOf(2.0));
-		assertEquals(ord.indices(1),new Pair<Integer,Integer>(0,1));
-		
-		assertEquals(ord.getDistancia(2),Double.valueOf(2.2));
-		assertEquals(ord.indices(2),new Pair<Integer,Integer>(1,2));
-		
-		assertEquals(ord.getDistancia(3),Double.valueOf(2.3));
-		assertEquals(ord.indices(3),new Pair<Integer,Integer>(3,0));
-		
-		
+		Assert.iguales(ord, indiceEsperado,esperado);
 		
 	}
 
