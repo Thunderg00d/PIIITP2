d106f4e59c14fb02912478d50387774a2532ec6a
Seba <sebithaa1999@gmail.com>
2019-10-08 21:27:58 -0300

    Peso de cada cluster.

--- JmapViewer/src/clustering/Clustering.java
+++ JmapViewer/src/clustering/Clustering.java
@@ -1,70 +1,115 @@
 package clustering;
 
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
+import grafo.BFS;
 import grafo.Grafo;
 import javafx.util.Pair;
 import ordenar.Ordenar;
 
 public class Clustering {
 
-	private List<Pair<Integer,Integer>>aristasAEliminar;
+	private List<Pair<Integer, Integer>> aristasAEliminar;
+	private HashMap<Double, ArrayList<Integer>> clus = new HashMap<Double, ArrayList<Integer>>();
+
 	private int cantClusters = 1;
 	private Grafo grafo;
-	
+
 	public Clustering(Grafo g) {
-		grafo=g;
-		aristasAEliminar=new ArrayList<Pair<Integer,Integer>>();
+		grafo = g;
+		aristasAEliminar = new ArrayList<Pair<Integer, Integer>>();
 	}
+
 	public void clusteringAristasMasPesadas(int cantidadClusters) {
-		corroborarCantidad(cantidadClusters,grafo.cantAristas());
-	    reiniciarValores();
-		eliminarAristas(cantidadClusters,grafo.getDistancias(),grafo.getIndices());
-		cantClusters+=aristasAEliminar.size();
+		corroborarCantidad(cantidadClusters, grafo.cantAristas());
+		reiniciarValores();
+		eliminarAristas(cantidadClusters, grafo.getDistancias(), grafo.getIndices());
+		cantClusters += aristasAEliminar.size();
 	}
-	private void eliminarAristas(int cantidadClusters,Double[]distancias,List<Pair<Integer,Integer>>indices) {
-		Ordenar ordenar=new Ordenar(distancias,indices);
-	    int aristasEliminadas=0;
-		for(int i=ordenar.tamano()-1;i>=0;i--) {
-			if(aristasEliminadas==cantidadClusters-1)
+
+	private void eliminarAristas(int cantidadClusters, Double[] distancias, List<Pair<Integer, Integer>> indices) {
+		Ordenar ordenar = new Ordenar(distancias, indices);
+		int aristasEliminadas = 0;
+		for (int i = ordenar.tamano() - 1; i >= 0; i--) {
+			if (aristasEliminadas == cantidadClusters - 1)
 				break;
 			aristasAEliminar.add(ordenar.indices(i));
 			aristasEliminadas++;
-		
+
 		}
-		for(int i=0;i<aristasAEliminar.size();i++) {
-			Integer x=aristasAEliminar.get(i).getKey();
-			Integer y=aristasAEliminar.get(i).getValue();
-			if(x.equals(y)) {
-					Pair<Integer,Integer>indiceVecinoMasPesado=grafo.vecinoMasPesado(x);
-					grafo.borrarArista(indiceVecinoMasPesado.getKey(),indiceVecinoMasPesado.getValue());
+		BFS bfs = new BFS(grafo);
+		for (int i = 0; i < aristasAEliminar.size(); i++) {
+			/*
+			 * if(x.equals(y)) {
+			 * Pair<Integer,Integer>indiceVecinoMasPesado=grafo.vecinoMasPesado(x);
+			 * grafo.borrarArista(indiceVecinoMasPesado.getKey(),indiceVecinoMasPesado.
+			 * getValue()); } else grafo.borrarArista(aristasAEliminar.get(i).getKey(),
+			 * aristasAEliminar.get(i).getValue()); }
+			 */
+
+			grafo.borrarArista(aristasAEliminar.get(i).getKey(), aristasAEliminar.get(i).getValue());
+
+			if (clus.size() == 0) {
+				Double num1 = bfs.pesoSubgrafo(aristasAEliminar.get(i).getKey());
+				Double num2 = bfs.pesoSubgrafo(aristasAEliminar.get(i).getValue());
+				clus.put(num1, bfs.verticesDelSubgrafo(aristasAEliminar.get(i).getKey()));
+				clus.put(num2, bfs.verticesDelSubgrafo(aristasAEliminar.get(i).getValue()));
+			} else {
+
+				HashMap<Double, ArrayList<Integer>> copy = clonarSet(clus);
+				System.out.println(copy.toString());
+				for (Entry<Double, ArrayList<Integer>> entry : copy.entrySet()) {
+					bfs = new BFS(grafo);
+					if (entry.getValue().contains(aristasAEliminar.get(i).getKey())) {
+						Double num1 = bfs.pesoSubgrafo(aristasAEliminar.get(i).getKey());
+						Double num2 = bfs.pesoSubgrafo(aristasAEliminar.get(i).getValue());
+						clus.remove(entry.getKey());
+						clus.put(num1, bfs.verticesDelSubgrafo(aristasAEliminar.get(i).getKey()));
+						clus.put(num2, bfs.verticesDelSubgrafo(aristasAEliminar.get(i).getValue()));
+					}
+
+				}
 			}
-			else
-					grafo.borrarArista(aristasAEliminar.get(i).getKey(), aristasAEliminar.get(i).getValue());
+
 		}
+		System.out.println("CLUSTERS: " + clus.keySet().toString());
+		cantClusters += aristasAEliminar.size();
+
 	}
+
+	private HashMap<Double, ArrayList<Integer>> clonarSet(HashMap<Double, ArrayList<Integer>> clus2) {
+		HashMap<Double, ArrayList<Integer>> copia = new HashMap<Double, ArrayList<Integer>>();
+		for (Entry<Double, ArrayList<Integer>> entry : clus2.entrySet()) {
+			copia.put(entry.getKey(), entry.getValue());
+		}
+		return copia;
+	}
+
 	public void clusteringVecinosMasPesados(int cantidadClusters) {
-		corroborarCantidad(cantidadClusters,grafo.tamano());
+		corroborarCantidad(cantidadClusters, grafo.tamano());
 		reiniciarValores();
-		Double[]promedioVecinos=new Double[grafo.tamano()];
-		List<Pair<Integer,Integer>>indices=new ArrayList<Pair<Integer,Integer>>();
-		for(int i=0;i<grafo.tamano();i++) {
-				promedioVecinos[i]=grafo.promedioVecinos(i);
-				indices.add(new Pair<Integer,Integer>(i,i));
+		Double[] promedioVecinos = new Double[grafo.tamano()];
+		List<Pair<Integer, Integer>> indices = new ArrayList<Pair<Integer, Integer>>();
+		for (int i = 0; i < grafo.tamano(); i++) {
+			promedioVecinos[i] = grafo.promedioVecinos(i);
+			indices.add(new Pair<Integer, Integer>(i, i));
 		}
-		eliminarAristas(cantidadClusters,promedioVecinos,indices);
-		
+		eliminarAristas(cantidadClusters, promedioVecinos, indices);
+
 	}
+
 	private void reiniciarValores() {
 		aristasAEliminar.clear();
 	}
-	
-	private void corroborarCantidad(int cantidadClusters,int maximaCantidad) {
-		if(cantidadClusters>maximaCantidad)
+
+	private void corroborarCantidad(int cantidadClusters, int maximaCantidad) {
+		if (cantidadClusters > maximaCantidad)
 			throw new IllegalArgumentException("La cantidad de clusters debe ser menor a la cantidad de aristas");
 	}
+
 	public int cantClusters() {
 		return cantClusters;
 	}
---
+++ JmapViewer/src/grafo/BFS.java
@@ -0,0 +1,117 @@
+package grafo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BFS
+{
+	// Grafo
+	private Grafo grafo;
+	
+	// Auxiliares para BFS
+	private Set<Integer> pendientes;
+	private boolean[] marcados;
+	private Double pesoTotal = 0.0;
+	
+	public BFS(Grafo g)
+	{
+		grafo = g;
+	}
+
+	// Determina si el grafo es conexo
+	public boolean esConexo()
+	{
+		inicializarAuxiliares();
+		
+		while( pendientes.size() != 0 )
+		{
+			System.out.println(pendientes.toString());
+			int i = seleccionarPendiente();
+			marcados[i] = true;
+			
+			pendientes.addAll(vecinosNoMarcados(i));
+			pendientes.remove(i);
+		}
+			pesoTotal=0.0;
+		return todosMarcados();
+	}
+	public double pesoSubgrafo(int j) {
+		inicializarAuxiliares(j);
+		while( pendientes.size() != 0 )
+		{
+			int i = seleccionarPendiente();
+			marcados[i] = true;
+			pendientes.addAll(vecinosNoMarcados(i));
+			pendientes.remove(i);
+		}
+		double temp = pesoTotal;
+			pesoTotal=0.0;
+		return temp;
+	}
+	public ArrayList<Integer> verticesDelSubgrafo(int j){
+		ArrayList<Integer> vertices = new ArrayList<Integer>();
+		inicializarAuxiliares(j);
+		while( pendientes.size() != 0 )
+		{
+			int i = seleccionarPendiente();
+			marcados[i] = true;
+			pendientes.addAll(vecinosNoMarcados(i));
+			vertices.addAll(vecinosNoMarcados(i));
+			pendientes.remove(i);
+		}
+		vertices.add(j);
+		return vertices;
+	}
+	
+	// Inicializa los elementos auxiliares
+	private void inicializarAuxiliares()
+	{
+		pendientes = singleton(0);
+		marcados = new boolean[grafo.tamano()]; // Todos false
+	}
+	private void inicializarAuxiliares(int j)
+	{
+		pendientes = singleton(j);
+		marcados = new boolean[grafo.tamano()]; // Todos false
+	}
+
+	// Vecinos no marcados de un vertice
+	private Set<Integer> vecinosNoMarcados(int i)
+	{
+		Set<Integer> ret = new HashSet<Integer>();
+		for(Integer j: grafo.vecinos(i) ) if( marcados[j] == false ) {
+			pesoTotal+=grafo.getArista(i, j);
+			ret.add(j);
+	}
+		return ret;
+	}
+
+	// Construye un set con el parametro como unico elemento
+	private Set<Integer> singleton(int elemento)
+	{
+		Set<Integer> L = new HashSet<Integer>();
+		L.add(elemento);
+		
+		return L;
+	}
+
+	// Determina estan todos marcados
+	private boolean todosMarcados()
+	{
+		int i = 0;
+		while( i < marcados.length && marcados[i] == true )
+			++i;
+		
+		return i == marcados.length;
+	}
+
+	// Obtiene un elemento del set, si no esta vacio
+	private int seleccionarPendiente()
+	{
+		for(Integer elemento: pendientes)
+			return elemento;
+		
+		throw new IllegalArgumentException("El set esta vacio!");
+	}
+}
